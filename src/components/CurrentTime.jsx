693799306ef03d870d0ed7f1e982194cd9d63210
mananjani2102 <manan.jani.cg@gmail.com>
2026-03-02 18:40:19 +0530

    add CurrentTime component to display live time

---
+++ src/components/CurrentTime.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function CurrentTime() {
+    let time = new Date();
+    return (
+        <p className='lead'>This is the current time :{time.toLocaleDateString()}- {time.toLocaleTimeString()}</p>
+    );
+}
+
+export default CurrentTime;
