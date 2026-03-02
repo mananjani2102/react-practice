a551ad8b16513d77caf614e2c02a3c8bf5950db9
mananjani2102 <manan.jani.cg@gmail.com>
2026-03-02 18:40:17 +0530

    add root App React component

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import ClockHeading from './Components/ClockHeading'
+import ClockSlogan from './Components/ClockSlogan'
+import CurrentTime from './Components/CurrentTime'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+
+  return (
+    <center>
+      <ClockHeading></ClockHeading>
+      <ClockSlogan></ClockSlogan>
+      <CurrentTime></CurrentTime>
+    </center>
+  );
+}
+
+export default App
