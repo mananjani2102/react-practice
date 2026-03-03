62b7d7ddb9194f0682c3fc3617af6f69a11363cf
mananjani2102 <manan.jani.cg@gmail.com>
2026-03-03 23:37:47 +0530

    update item component for learning fragment

--- Fragments/learning-fragment/src/components/item.jsx
+++ Fragments/learning-fragment/src/components/item.jsx
@@ -1,8 +1,10 @@
-function item( {foodItem}) {
+import styles from "./Item.module.css";
+
+function item({ foodItem }) {
   return (
-      <li className="list-group-item">
-        {foodItem}
-      </li>
+    <li className={`${Styles["kg-item"]} list-group-item`}>
+      <span className={styles["kg-span"]}>{foodItem}</span>
+    </li>
   );
 }
 
