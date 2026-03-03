d1f60a18e436dcb06192b5d654e8748a1e9f94ac
mananjani2102 <manan.jani.cg@gmail.com>
2026-03-03 23:37:46 +0530

    update main app component for learning fragment

--- Fragments/learning-fragment/src/App.jsx
+++ Fragments/learning-fragment/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <div>
-      <h1>Healthy Food</h1>
+      <h1 className="food-heading">Healthy Food</h1>
       <ErrorMessage items = {foodItems}></ErrorMessage>
       <FoodItems items = {foodItems}></FoodItems>
     </div>
