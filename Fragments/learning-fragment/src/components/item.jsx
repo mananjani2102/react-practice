418c040fdb721fd21e35c877ccc99a31d439f170
mananjani2102 <manan.jani.cg@gmail.com>
2026-03-06 11:37:04 +0530

    fix CSS module import case, add Buy button with click handler

--- Fragments/learning-fragment/src/components/item.jsx
+++ Fragments/learning-fragment/src/components/item.jsx
@@ -1,9 +1,16 @@
-import styles from "./Item.module.css";
+import styles from "./item.module.css";
 
 function item({ foodItem }) {
+
+  const handleBuyButtonClicked = (foodItem) =>{
+    console.log(`${foodItem}being bought`)
+  }
   return (
-    <li className={`${Styles["kg-item"]} list-group-item`}>
+    <li className={`${styles["kg-item"]} list-group-item`}>
       <span className={styles["kg-span"]}>{foodItem}</span>
+      <button className={`${styles.button} btn btn-info`}
+      onClick={()=> handleBuyButtonClicked (foodItem)}
+      >Buy</button>
     </li>
   );
 }
