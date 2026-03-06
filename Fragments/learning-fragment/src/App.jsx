afd1d04932314c3dd9ad489dc8afc8e67fce5fa3
mananjani2102 <manan.jani.cg@gmail.com>
2026-03-06 11:37:03 +0530

    add Container wrapper and Fragment syntax to App component

--- Fragments/learning-fragment/src/App.jsx
+++ Fragments/learning-fragment/src/App.jsx
@@ -2,16 +2,23 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import FoodItems from "./components/FoodItems";
 import ErrorMessage from "./components/ErrorMessage";
+import Container from "./components/Container";
 
 function App() {
   let foodItems = ["Sabzi", "Green Vegetable", "Roti", "Salad", "Milk"];
 
   return (
-    <div>
-      <h1 className="food-heading">Healthy Food</h1>
-      <ErrorMessage items = {foodItems}></ErrorMessage>
-      <FoodItems items = {foodItems}></FoodItems>
-    </div>
+    <>
+      <Container>
+        <h1 className="food-heading">Healthy Food</h1>
+        <ErrorMessage items={foodItems}></ErrorMessage>
+        <FoodItems items={foodItems}></FoodItems>
+      </Container>
+      {/* <Container>
+        <p>Above is the list of healthy foods that are good for your health and well being.</p>
+      </Container> */}
+
+    </>
   );
 }
 
